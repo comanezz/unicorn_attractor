6e7d5e5c5c7bfce1ae483ba41ae70a577dd568be
comanezz <aymeric.lcharles@gmail.com>
2020-05-18 20:42:05 +0100

    Static_root added for heroku deployement

--- unicorn_attractor/settings.py
+++ unicorn_attractor/settings.py
@@ -139,4 +139,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
+# Temporary static root for heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
