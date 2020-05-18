6a8a40b240552e12a74a65277d60be95d0ce424d
comanezz <aymeric.lcharles@gmail.com>
2020-05-18 20:32:02 +0100

    inital commit

---
+++ accounts/forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+
+class UserLoginForm(forms.Form):
+    """Form to be used to log users in"""
+
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(UserCreationForm):
+    """Form used to register a new user"""
+
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Password Confirmation", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(email=email).exclude(username=username):
+            raise forms.ValidationError(u'Email address must be unique')
+        return email
+
+    """
+        Allow me to make the email field mandatory
+        Source: https://stackoverflow.com/questions/5493096/django-user-model-email-field-how-to-make-it-mandatory
+    """
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['email'].required = True
+
+    """
+        It was clean_password2 from the lesson but I remove the number 2
+        that allow me use password strengh otherwise it wasn't working and it shows error message now.
+    """
+    def clean_password(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if not password1 or not password2:
+            raise ValidationError("Please confirm your password")
+        
+        if password1 != password2:
+            raise ValidationError("Passwords must match")
+        
+        return password2
