6e6e6395ec728b7cf75fe9cbe001670a0909a8d1
comanezz <aymeric.lcharles@gmail.com>
2020-05-21 00:59:24 +0100

    New create_or_edit view created, Urls set up

--- bugs/urls.py
+++ bugs/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from .views import all_bugs
+from .views import all_bugs, create_or_edit_bug
 
 urlpatterns = [
     url(r'^$', all_bugs, name='all_bugs'),
+    url(r'^new/$', create_or_edit_bug, name='new_bug'),
+    url(r'^(?P<pk>\d+)/edit/$', create_or_edit_bug, name='edit_bug')
 ]
--- bugs/views.py
+++ bugs/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Bug
+from .forms import BugForm
 
 # Create your views here.
 def all_bugs(request):
+    """ View all bug list
+    """
     bugs = Bug.objects.all()
     return render(request, "bugs.html", {"bugs": bugs})
+
+@login_required
+def create_or_edit_bug(request, pk=None):
+    """
+    Create a view that allows us to create
+    or edit a bug depending if the Post ID
+    is null or not
+    """
+    bug = get_object_or_404(Bug, pk=pk) if pk else None
+    if request.method == "POST":
+        form = BugForm(request.POST, instance=bug)
+        if form.is_valid():
+            bug = form.save(commit=False)
+            bug.author = request.user
+            bug.save()
+            # Need to change the redirect to bug detail when I have created it.
+            return redirect(all_bugs)
+    else:
+        form = BugForm(instance=bug)
+
+    return render(request, 'bugform.html', {'form': form})
