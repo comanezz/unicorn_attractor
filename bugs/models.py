962e7099580b4c27dd788aecb76db85b640fed70
comanezz <aymeric.lcharles@gmail.com>
2020-05-20 20:23:44 +0100

    Bug model created

--- bugs/models.py
+++ bugs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Bug(models.Model):
+    title = models.CharField(max_length=150, default='', blank=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
