75d78d65448ef083a6f88de5ef6913bd940459e6
comanezz <aymeric.lcharles@gmail.com>
2020-05-20 22:25:47 +0100

    Add status to Bug model

--- bugs/models.py
+++ bugs/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Bug(models.Model):
     title = models.CharField(max_length=150, default='', blank=False)
     description = models.TextField()
+
+    STATUS_CHOICES = (
+        ('to do', 'to do'),
+        ('Doing', 'Doing'),
+        ('Done', 'Done')
+    )
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='to do')
 
     def __str__(self):
         return self.title
