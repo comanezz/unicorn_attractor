5fa10d152dbafc8e3742390e4fa99aee7c2d5a95
comanezz <aymeric.lcharles@gmail.com>
2020-05-20 10:49:23 +0100

    SMTP gmail added

--- unicorn_attractor/settings.py
+++ unicorn_attractor/settings.py
@@ -161,3 +161,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
