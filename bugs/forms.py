78725afffaea1f58f90cfdc52229339759463887
comanezz <aymeric.lcharles@gmail.com>
2020-05-27 15:39:57 +0100

    Label for the context field in CommentForm has been removed

--- bugs/forms.py
+++ bugs/forms.py
@@ -10,6 +10,7 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('context',)
+        labels = {'context': ''}
         widgets = {
         'context': forms.Textarea(attrs={'placeholder': 'Comment here', 'rows': '2'}),
         }
