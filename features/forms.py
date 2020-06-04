260e14203ee702d5afb53c69a4830718cb6b0524
comanezz <aymeric.lcharles@gmail.com>
2020-06-04 22:10:10 +0100

    Comment form created

--- features/forms.py
+++ features/forms.py
@@ -1,7 +1,16 @@
 from django import forms
-from .models import Feature
+from .models import Feature, Comment
 
 class FeatureForm(forms.ModelForm):
     class Meta:
         model = Feature
         fields = ('title', 'description')
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('context',)
+        labels = {'context': ''}
+        widgets = {
+        'context': forms.Textarea(attrs={'placeholder': 'Comment here', 'rows': '3', 'cols': '120'}),
+        }
