8933573a9ebfc9c88aade4d2d361aafcf0165a8c
comanezz <aymeric.lcharles@gmail.com>
2020-06-04 20:54:32 +0100

    Create or edit url and view created.

--- features/urls.py
+++ features/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import all_features, feature_detail
+from .views import all_features, feature_detail, create_or_edit_feature
 
 urlpatterns = [
     url(r'^$', all_features, name='all_features'),
     url(r'^(?P<pk>\d+)/(?P<slug>[\w-]+)/$', feature_detail, name='feature_detail'),
+    url(r'^new/$', create_or_edit_feature, name='new_feature'),
 ]
--- features/views.py
+++ features/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import *
+from .forms import *
 from django.contrib import messages
 from django.db.models import Count
 
@@ -14,19 +15,19 @@ def all_features(request):
 def feature_detail(request, pk, slug):
     """
     Create a view that returns a single
-    Bug object based on the bug ID (pk) and
-    render it to the 'bugdetail.html' template.
+    feature object based on the feature ID (pk) and
+    render it to the 'featuredetail.html' template.
     Or return a 404 error if the post is not found
     """
-    bug = get_object_or_404(Bug, pk=pk, slug=slug)
+    feature = get_object_or_404(feature, pk=pk, slug=slug)
     # Upvoted part
     is_upvoted = False
-    # Checks if current user had previously upvoted the bug
-    if bug.upvotes.filter(id=request.user.id).exists():
+    # Checks if current user had previously upvoted the feature
+    if feature.upvotes.filter(id=request.user.id).exists():
         is_upvoted = True
 
     # Comment part
-    comments = Comment.objects.filter(bug=bug, reply=None).order_by('id')
+    comments = Comment.objects.filter(feature=feature, reply=None).order_by('id')
 
     if request.method == 'POST':
         comment_form = CommentForm(request.POST or None)
@@ -38,17 +39,46 @@ def feature_detail(request, pk, slug):
             if reply_id:
                 comment_qs = Comment.objects.get(id=reply_id)
 
-            comment = Comment.objects.create(bug=bug, author=request.user, context=context, reply=comment_qs)
+            comment = Comment.objects.create(feature=feature, author=request.user, context=context, reply=comment_qs)
             comment.save()
-            return redirect(bug.get_absolute_url())
+            return redirect(feature.get_absolute_url())
     else:
         comment_form = CommentForm()
 
     context = {
-        'bug': bug,
+        'feature': feature,
         'is_upvoted': is_upvoted,
         'comments': comments,
         'comment_form': comment_form
         }
 
-    return render(request, "bugdetail.html", context)
+    return render(request, "featuredetail.html", context)
+
+@login_required
+def create_or_edit_feature(request, pk=None, slug=None):
+    """
+    Create a view that allows us to create
+    or edit a feature depending if the Post ID
+    is null or not
+    """
+    feature = get_object_or_404(Feature, pk=pk) if pk else None
+
+    # if the user is not equal to the creator of the feature post he will not be able to edit the post
+    if feature is not None:
+        if feature.author != request.user:
+            messages.error(request, "This is not your post, you can't edit", extra_tags="alert alert-danger")
+            return redirect(feature.get_absolute_url())
+
+    if request.method == "POST":
+        form = FeatureForm(request.POST, instance=feature)
+        if form.is_valid():
+            feature = form.save(commit=False)
+            feature.author = request.user
+            feature.save()
+            # Uncomment once I set up feature detail view correctly
+            # return redirect(feature.get_absolute_url())
+            return redirect(all_features)
+    else:
+        form = FeatureForm(instance=feature)
+
+    return render(request, 'featureform.html', {'form': form})
