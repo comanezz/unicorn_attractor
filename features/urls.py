c184872ec1b85ec3add130fe6865113fe9f5eab9
comanezz <aymeric.lcharles@gmail.com>
2020-06-04 22:11:18 +0100

    New url added. edit feature and upvote feature

--- features/urls.py
+++ features/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-from .views import all_features, feature_detail, create_or_edit_feature
+from .views import all_features, feature_detail, create_or_edit_feature, upvote_feature
 
 urlpatterns = [
     url(r'^$', all_features, name='all_features'),
     url(r'^(?P<pk>\d+)/(?P<slug>[\w-]+)/$', feature_detail, name='feature_detail'),
     url(r'^new/$', create_or_edit_feature, name='new_feature'),
+    url(r'^(?P<pk>\d+)/(?P<slug>[\w-]+)/edit/$', create_or_edit_feature, name='edit_feature'),
+    url(r'^upvote_feature/$', upvote_feature, name="upvote_feature"),
 ]
