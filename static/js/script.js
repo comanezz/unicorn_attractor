503aa32a70a4a890a5d9e9f2601d5e36174319f4
comanezz <aymeric.lcharles@gmail.com>
2020-06-29 19:26:27 +0100

    Comment added

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // This function allows to show the reply section when clicking on reply btn
     $('.reply-btn').click(function(){
         $(this).parent().parent().parent().next('.replied-comments-section').fadeToggle();
     });
